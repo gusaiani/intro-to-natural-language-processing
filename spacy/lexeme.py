e57f4089719acb6bfd7b800872c4dd19b7c21593
Gustavo Saiani <gustavo@poe.ma>
2018-12-22 21:06:28 -0200

    Add lexeme

---
+++ spacy/lexeme.py
@@ -0,0 +1,18 @@
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(u'I love coffee')
+
+for word in doc:
+    lexeme = doc.vocab[word.text]
+    print(
+        lexeme.text,
+        lexeme.orth,
+        lexeme.shape_,
+        lexeme.prefix_,
+        lexeme.suffix_,
+        lexeme.is_alpha,
+        lexeme.is_digit,
+        lexeme.is_title,
+        lexeme.lang_
+    )
